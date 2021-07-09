ccb2752e3f5f780e63fc336857cc7c07e68fbf65
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2021-07-09 20:46:16 +0900

    Upload week20 Algorithm code

---
+++ week20/17298_오큰수_hyeinlee725.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(sys.stdin.readline()) # 수열의 크기
+# 수열의 원소
+element = list(map(int, sys.stdin.readline().split()))
+stack = []
+result = [-1 for _ in range(N)]
+
+for i in range(len(element)):
+    # 해당 범위 내에 있고, (i < N, stack내에)
+    # element[stack[-1]이 element[i]보다 작으면
+    while i < N and stack and element[stack[-1]] < element[i]:
+        # stack을 pop, result에 element값을 넣어줌
+        result[stack.pop()] = element[i]\
+    # 아니면 i를 stack에 넣어줌
+    stack.append(i)
+    i += 1
+for i in range(N):
+    print(result[i], end = ' ')
