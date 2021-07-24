058da891da1a3919b31743123b237243ed0e561e
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2021-07-24 22:44:49 +0900

    Update 오픈채팅방_hyeinlee725.py

--- week22/오픈채팅방_hyeinlee725.py
+++ week22/오픈채팅방_hyeinlee725.py
@@ -9,10 +9,10 @@ def solution(record):
             nickname = data[2] # 가상의 닉네임
             user[user_id] = nickname # 저장된 nickname 변경
         if (action in ("Enter", "Leave")):
-            actions.append((action, user_id)) # user id와 함께 추가
+            log.append((action, user_id)) # user id와 함께 추가
 
     # 채팅방 메시지 출력
-    for action, user_id in actions:
+    for action, user_id in log:
         if (action == "Enter"):
             answer.append(user[user_id] + "님이 들어왔습니다.")
         else:
