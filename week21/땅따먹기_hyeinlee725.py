d744624e9d16fed3362cc83e1f6fc1af9849fa6c
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2021-07-24 23:47:37 +0900

    Update 땅따먹기_hyeinlee725.py

--- week21/땅따먹기_hyeinlee725.py
+++ week21/땅따먹기_hyeinlee725.py
@@ -6,8 +6,8 @@ def solution(land):
         dp[0][i] = land[0][i]
 
     # 현재 행의 점수와 현재 행의 다음 열과 같지 않은 가장 큰 수를 더함
-    for i in range(1, len(land)):
+    for i in range(1, N):
         for j in range(4):
-            land[i][j] += max(land[i - 1][:j] + land[i - 1][j + 1:])
-        answer = max(land[len(land) - 1]) # 최대값 반환
+            dp[i][j] = land[i][j] + max(dp[i - 1][:j] + dp[i - 1][j + 1:])
+        answer = max(dp[len(land) - 1]) # 최대값 반환
     return answer
